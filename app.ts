081060b7f58f5e1bdc7ce9e484e5fba096252a81
Justin Schieck <jschieck@miovision.corp>
2021-09-13 12:25:34 -0400

    finished section 2

--- app.ts
+++ app.ts
@@ -9,7 +9,10 @@ if(typeof userInput === 'string'){
     userName = userInput; // will clear non type errors through type checks
 }
 
-//never type
+// never type
+// alternative to void, stops from the random undefined being thrown after call
+// mainly used for code quality over void, usually it would infer void if not set
+// good for stuff like error generation
 function generateError(message: string, errorCode: number): never{
     throw {message, errorCode};
 }
